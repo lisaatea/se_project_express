75e45413dbd8b015bcb4afa652fb03bb43c6746c
lisaatea <lisaatea@gmail.com>
2024-07-18 17:24:02 -0700

    remove crash test

--- app.js
+++ app.js
@@ -23,12 +23,6 @@ app.use(express.json());
 
 app.use(requestLogger);
 
-app.get("/crash-test", () => {
-  setTimeout(() => {
-    throw new Error("Server will crash now");
-  }, 0);
-});
-
 app.use("/", indexRouter);
 
 app.use(errorLogger);
